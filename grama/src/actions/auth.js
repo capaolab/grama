f683f400ad9228a8bd0f6b9eff757e3a9d91651a
viriatonova <viriatonova@capaolab.com>
2024-11-15 08:36:32 -0300

    up actions

--- grama/src/actions/auth.js
+++ grama/src/actions/auth.js
@@ -1,13 +1,19 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import loginSchema from "@/lib/schemas/loginSchema"
-export async function login(state, formData) {
-  try {
-    if (!formData.email || !formData.password) return
-    const validatedField = await loginSchema.validate(formData)
-    if (!validatedField) return
-    console.log("VALIDAÇÕO DE DADOS SUCESSO")
-  } catch (error) {
-    return error
-  }
-  return
+async function auth(state, formData) {
+  // if (!formData.email || !formData.password) {
+  //   return state.errors.name = "Preencha todos os campos"
+  // }
+  // const validatedField = await loginSchema.validate(formData)
+  // if (!validatedField) {
+  //   return state.errors.name = "Preencha todos os campos"
+  // }
+  setTimeout(() => {
+    console.log('loading')
+  }, 3000)
+  const email = formData.get('email')
+  const password = formData.get('password')
+  console.log(email, password)
+  return 'LOGIN OK'
 }
+
+export default auth
