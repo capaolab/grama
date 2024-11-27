ac0b084516126379d56123e4b3e2007455b179e5
Rodrigo <rodrigojsg9@gmail.com>
2024-11-27 20:54:50 -0300

    feat: added new componentes in dashboard page

--- grama/src/app/dashboard/page.jsx
+++ grama/src/app/dashboard/page.jsx
@@ -1,14 +1,47 @@
 import DropDown from "@/components/DropDown";
 import SearchInput from "@/components/SearchInput";
+import { HiOutlineInbox } from "react-icons/hi";
+import { FaCirclePlus } from "react-icons/fa6";
+import { LuCalendarDays } from "react-icons/lu";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 function Dashboard() {
   return (
     <div className="w-full flex text-[var(--text-color)] my-1 flex-col">
       <header className="flex gap-3 h-20 w-full items-center px-4 border-b border-[var(--border-color)]">
         <DropDown />
-        <SearchInput />
+        <div className="flex items-center gap-2 ml-auto">
+          <SearchInput />
+          <div className="ml-10 flex items-center gap-2">
+
+            <div className="relative flex items-center justify-center w-10 h-10 border border-[var(--border-color)] cursor-pointer rounded-full">
+              <div className="h-2 w-2 absolute top-2 right-2 bg-[var(--bg-sidebar)] bg-green-500 rounded-full">
+              </div>
+              <HiOutlineInbox className="text-2xl text-[var(--text-color)]" />
+            </div>
+
+            <div className="relative flex gap-2 items-center justify-center text-[var(--text-color)] w-max h-10 px-3 border border-[var(--border-color)] cursor-pointer rounded-full">
+              <span className="font-medium text-base">Add new product</span>
+              <FaCirclePlus className="text-[var(--text-color)]" size={20} />
+            </div>
+
+          </div>
+        </div>
       </header>
-      <h1>Dashboard</h1>
+      <div className="p-4 h-full w-[75%] border-r border-[var(--border-color)]">
+        <div className="flex justify-between">
+          <div>
+            <h1>Dashboard</h1>
+            <h3 className="text-xl">An any way to manage sales with care and precision.</h3>
+          </div>
+
+          <div className="flex items-center gap-2 h-10 w-max px-3 mt-4 border border-[var(--border-color)] cursor-pointer rounded-full">
+            <LuCalendarDays className="text-[var(--text-color)]" size={20} />
+            <span className="font-medium text-base">January 2024 - May 2024</span>
+            <MdOutlineKeyboardArrowDown className="dropdown-arrow text-2xl transition duration-300" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- grama/src/components/SearchInput.jsx
+++ grama/src/components/SearchInput.jsx
@@ -2,9 +2,12 @@ import { LuSearch } from "react-icons/lu";
 
 function SearchInput() {
   return (
-    <div className="flex intems-center text-[var(--text-color)] justify-center w-96 h-10 border border-[var(--border-color)] rounded-full">
-      <input type="text" placeholder="Search" className="w-full h-full max-w-xs bg-[var(--background)] outline-none appearance-none" />
-      <LuSearch className="text-2xl h-full flex tems-center" />
+    <div
+      className="relative flex intems-center bg-[var(--background)]
+      text-[var(--text-color)] justify-center w-96 h-10 border border-[var(--border-color)] rounded-full"
+    >
+      <input type="text" placeholder="Search" className="absolute inset-0 px-4 bg-transparent z-10 outline-none appearance-none" />
+      <LuSearch className="text-2xl absolute right-4 ml-auto h-full flex tems-center" />
     </div>
   )
 
